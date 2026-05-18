4d70e8d02f7301df449bb39fc05079f3b2fb6d77
leoluopy <leo_luopy@139.com>
2026-05-18 14:23:09 +0800

    for down

--- modeling/down_weights.py
+++ modeling/down_weights.py
@@ -3,7 +3,9 @@ from modelscope import snapshot_download
 # model_dir = snapshot_download('deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct', cache_dir='./')
 # model_dir = snapshot_download('Qwen/Qwen2.5-0.5B-Instruct', cache_dir='./')
 # model_dir = snapshot_download('deepseek-ai/DeepSeek-V4-Pro', cache_dir='./')
-model_dir = snapshot_download('deepseek-ai/DeepSeek-V3.2-Exp', cache_dir='./')
+# model_dir = snapshot_download('deepseek-ai/DeepSeek-V3.2-Exp', cache_dir='./')
+model_dir = snapshot_download('MiniMax/MiniMax-M2.7', cache_dir='./')
+# model_dir = snapshot_download('moonshotai/Kimi-K2.6', cache_dir='./')
 
 
 print('模型下载完成，保存路径：', model_dir)
